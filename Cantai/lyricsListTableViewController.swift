28c8cee6d8f4a08b6e62584241b94c743c793447
Raul <raulmo1337@gmail.com>
2016-08-09 00:51:14 -0300

    First push

---
+++ Cantai/lyricsListTableViewController.swift
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2015 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import Firebase
+
+class lyricsListTableViewController: UITableViewController {
+    
+    // MARK: Constants
+    let ListToUsers = "ListToUsers"
+    let ref = FIRDatabase.database().reference()
+    
+    // MARK: Properties
+    var items = [Music]()
+    var user: User!
+    var userCountBarButtonItem: UIBarButtonItem!
+    
+    // MARK: UIViewController Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Set up swipe to delete
+        tableView.allowsMultipleSelectionDuringEditing = false
+        
+        // User Count
+        userCountBarButtonItem = UIBarButtonItem(title: "1", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(lyricsListTableViewController.userCountButtonDidTouch))
+        userCountBarButtonItem.tintColor = UIColor.whiteColor()
+        navigationItem.leftBarButtonItem = userCountBarButtonItem
+        
+        user = User(uid: "FakeId", email: "hungry@person.food")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //        // 1
+        //        ref.observeEventType(.Value, withBlock: { snapshot in
+        //
+        //            // 2
+        //            var newItems = [GroceryItem]()
+        //
+        //            // 3
+        //            for item in snapshot.children {
+        //
+        //                // 4
+        //                let groceryItem = GroceryItem(snapshot: item as! FDataSnapshot)
+        //                newItems.append(groceryItem)
+        //            }
+        //
+        //            // 5
+        //            self.items = newItems
+        //            self.tableView.reloadData()
+        //        })
+        
+        ref.child("musics").queryOrderedByChild("name").observeEventType(.Value, withBlock: { snapshot in
+            var newItems = [Music]()
+            for item in snapshot.children {
+                let groceryItem = Music(snapshot: item as! FIRDataSnapshot)
+                newItems.append(groceryItem)
+            }
+            self.items = newItems
+            self.tableView.reloadData()
+        })
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+    }
+    
+    // MARK: UITableView Delegate methods
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath)
+        let groceryItem = items[indexPath.row]
+        
+        cell.textLabel?.text = groceryItem.name
+        cell.detailTextLabel?.text = groceryItem.addedByUser
+        
+        // Determine whether the cell is checked
+        //toggleCellCheckbox(cell, isCompleted: groceryItem.completed)
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // 1
+            let groceryItem = items[indexPath.row]
+            // 2
+            groceryItem.ref?.removeValue()
+        }
+    }
+    
+//    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        // 1
+//        let cell = tableView.cellForRowAtIndexPath(indexPath)!
+//        // 2
+//        var groceryItem = items[indexPath.row]
+//        // 3
+//        let toggledCompletion = !groceryItem.completed
+//        // 4
+//        toggleCellCheckbox(cell, isCompleted: toggledCompletion)
+//        // 5
+//        groceryItem.ref?.updateChildValues([
+//            "completed": toggledCompletion
+//            ])
+//    }
+    
+    func toggleCellCheckbox(cell: UITableViewCell, isCompleted: Bool) {
+        if !isCompleted {
+            cell.accessoryType = UITableViewCellAccessoryType.None
+            cell.textLabel?.textColor = UIColor.blackColor()
+            cell.detailTextLabel?.textColor = UIColor.blackColor()
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+            cell.textLabel?.textColor = UIColor.grayColor()
+            cell.detailTextLabel?.textColor = UIColor.grayColor()
+        }
+    }
+    
+    // MARK: Add Item
+    
+    @IBAction func addButtonDidTouch(sender: AnyObject) {
+        // Alert View for input
+        let alert = UIAlertController(title: "Grocery Item",
+                                      message: "Add an Item",
+                                      preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .Default) { (action: UIAlertAction!) -> Void in
+                                        
+                                        // 1
+                                        let name = alert.textFields![0]
+                                        let lyric = alert.textFields![1]
+                                        
+                                        // 2
+                                        let music = Music(name: name.text!, addedByUser: self.user.email, lyric: lyric.text! )
+                                        
+                                        // 3
+
+                                        let musicRef = self.ref.child("musics").child(name.text!.lowercaseString)
+                                        
+                                        // 4
+                                        musicRef.setValue(music.toAnyObject())
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+                              animated: true,
+                              completion: nil)
+    }
+    
+    func userCountButtonDidTouch() {
+        performSegueWithIdentifier(ListToUsers, sender: nil)
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Cantai' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Cantai
+
+	pod 'Firebase/Auth'
+	pod 'Firebase/Database'
+	pod 'Firebase/Messaging'
+	pod 'Firebase/Storage'
+  target 'CantaiTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CantaiUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
